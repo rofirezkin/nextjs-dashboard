1ec16c117e9fb2aadf18251ed4c09681a70616ef
rrezkin <rrezkin@pvg.co.id>
2024-05-10 04:03:19 +0700

    fi

--- auth.ts
+++ auth.ts
@@ -18,7 +18,6 @@ async function getUser(email: string): Promise<User | undefined> {
 
 export const {auth, signIn, signOut} = NextAuth({
 	...authConfig,
-
 	secret: process.env.AUTH_SECRET,
 	providers: [
 		Credentials({
